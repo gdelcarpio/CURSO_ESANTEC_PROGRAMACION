0e88527c39563c5b32b6089f7dc141b76d418c5c
unknown <Administrador@jlopezchavez.vivienda.gob.pe>
2016-02-02 14:38:26 -0500

    Primera clase

---
+++ clase1/index.php
@@ -0,0 +1,13 @@
+<h1>Clase 1</h1>
+<hr>
+<h3>Hoy dia es: </h3>
+<p>
+<?php
+
+	/* este es una forma 
+	de comentar en grandes
+	secciones de texto */
+	 
+	print date('d-m-Y h:i:s');
+?>
+</p>
---
+++ clase1/pizarra-bart.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sin título</title>
+</head>
+
+<body>
+
+<?php
+
+	for($i=1; $i<=500; $i++){
+	
+		echo "<p> $i Tengo que llegar temprano el próximo sábado</p>";
+	
+	}
+
+?>
+
+
+</body>
+</html>
